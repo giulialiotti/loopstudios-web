1fa30f65b3cccfd74e54d5c69f0338160865e8d4
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-06-07 19:49:19 -0300

    Allow direct import paths for data and hooks

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,9 @@ exports.onCreateWebpackConfig = ({ actions }) => {
       alias: {
         components: path.resolve(__dirname, "src/components"),
         assets: path.resolve(__dirname, "src/assets"),
-        sections: path.resolve(__dirname, "src/sections")
+        sections: path.resolve(__dirname, "src/sections"),
+        graphql: path.resolve(__dirname, "src/graphql"),
+        hooks: path.resolve(__dirname, "src/hooks")
       },
     },
   });
